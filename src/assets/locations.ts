3fc1098f3e914f0aaa11af683faa197b8dfa4cd0
Ilya Meerovich <ilya.meerov@gmail.com>
2024-12-07 22:53:36 -0800

    chore: add locations

--- src/assets/locations.ts
+++ src/assets/locations.ts
@@ -33,15 +33,23 @@ export const locations: Location[] = [
     coordinates: [21.936212068940627, 100.60619644140225],
     googleMapsLink: "https://maps.app.goo.gl/LrGBAbXBmB8siXSW6"
   },
-  // {
-  //   name: "Hekai"
-  // },
+  {
+    name: "Hekai",
+    type: LOCATION_TYPE.MOUNTAIN,
+    coordinates: [21.82245008046033, 100.45970994941935],
+    description: "A mountain area in Menghai County, Xishuangbanna. Although there is a specific Hekai Village, at least [according to Farmerleaf](https://www.farmer-leaf.com/products/spring-2023-hekai), Hekai is an area comprised of the villages Man Mai, Man Nong, and Banpen.",
+    googleMapsLink: "https://maps.app.goo.gl/D79FgdDoBJbsUECi8",
+  },
+  {
+    name: "Banpen",
+    type: LOCATION_TYPE.VILLAGE,
+    coordinates: [21.737346739785963, 100.53092059631028],
+  },
   {
     name: "Mengsong",
-    type: LOCATION_TYPE.VILLAGE,
-    coordinates: [21.493675359407998, 100.51355966758277],
-    description: "A famous tea village in Xishuangbanna. Along with other surrounding villages, this makes up a 'Mengsong area'.",
-    googleMapsLink: "https://maps.app.goo.gl/7cG2z4HKZds4ujBS9"
+    type: LOCATION_TYPE.MOUNTAIN,
+    coordinates: [22.05290918693693, 100.56069837236929],
+    googleMapsLink: "https://maps.app.goo.gl/SzktbSAFwKcnnDsUA",
   },
   {
     name: "Mangfei",
@@ -63,26 +71,52 @@ export const locations: Location[] = [
     googleMapsLink: "https://maps.app.goo.gl/PzHBhPtRQpaYbXpH6",
     description: "This town is the namesake of a factory as well as an entire tea growing region comprised of multiple villages, including the famous Bingdao. Better descriptions can be found [here](https://teadb.org/lincang/) and [here](https://teasenz.eu/blogs/tea-magazine/mengku)."
   },
+  {
+    name: "Bingdao",
+    type: LOCATION_TYPE.VILLAGE,
+    coordinates: [23.75384879892382, 99.8927834978433],
+    queryName: "Bingdaocun",
+    googleMapsLink: "https://maps.app.goo.gl/Q6yTF318AmC6tkDe7",
+    description: "A famous tea village in the Mengku region of Lincang. Bingdao is known for high quality material that gets quite expensive. Bingdao is a village but also a mini region composed of the five villages of Bingdao, Di Jie, Nuo Wu, Ba Wai, and Nan Po."
+  },
+  {
+    name: "Bulang Mountain",
+    type: LOCATION_TYPE.MOUNTAIN,
+    coordinates: [21.625772610541837, 100.4063712622973],
+    googleMapsLink: "https://maps.app.goo.gl/Y1D6KsXwEGqDgAET9",
+    description: "A famous mountain area in Xishuangbanna, this area is home to some of the most famous tea growing villages in the world, including Lao Ban Zhang, Ban Zhang, and Lao Man'E."
+  },
+  {
+    name: "Lao Banzhang",
+    type: LOCATION_TYPE.VILLAGE,
+    coordinates: [21.728816432499585, 100.49903864710413],
+    googleMapsLink: "https://maps.app.goo.gl/p5e5QaXtDhDwdEUs6",
+    description: "One of the most famous (and expensive!) tea villages in the world."
+  },
+  {
+    name: "Banzhang",
+    type: LOCATION_TYPE.VILLAGE,
+    coordinates: [21.784522289669713, 100.45766310767932],
+    googleMapsLink: "https://maps.app.goo.gl/dNhsR9gT9AyFV1QB7"
+  },
+  {
+    name: "Lao Man'E",
+    type: LOCATION_TYPE.VILLAGE,
+    coordinates: [21.657740132570172, 100.41700986470293],
+    googleMapsLink: "https://maps.app.goo.gl/Sgyq1iyeMmPWwxR66"
+  },
+  {
+    name: "Naka",
+    type: LOCATION_TYPE.VILLAGE,
+    coordinates: [22.192239786196204, 100.5565139835436],
+    googleMapsLink: "https://maps.app.goo.gl/suonMPgSbwhUG7Ry5",
+    queryName: "Naka"
+  },
   // {
-  //   name: "Bingdao",
+  //   name: "Jinggu",
+  //   type: LOCATION_TYPE.COUNTY,
   //   // ...
-  // },
-  // {
-  //   name: "Bulang Mountain",
-  //   type: LOCATION_TYPE.MOUNTAIN
-  // },
-  // {
-  //   name: "Banzhang",
-  //   type: LOCATION_TYPE.VILLAGE
-  // },
-  // {
-  //   name: "Lao Banzhang",
-  //   type: LOCATION_TYPE.VILLAGE
-  // },
-  // {
-  //   name: "Lao Man'E",
-  //   type: LOCATION_TYPE.VILLAGE
-  // },
+  // }
   {
     name: "Yiwu",
     coordinates: [21.978524761501593, 101.47108940679107],
